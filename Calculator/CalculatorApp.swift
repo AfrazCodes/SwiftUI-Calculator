0507e4fa01e91583323111ee90d8c4a38b1f3e14
Afraz Siddiqui <afsiddiq@microsoft.com>
2021-03-05 09:26:32 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Afraz Siddiqui on 3/5/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
